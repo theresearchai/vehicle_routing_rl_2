de297e73056e200fcdd64ea73f998eca4da0ac9b
mayurgpt07 <mayurgpt07@gmail.com>
2021-03-12 16:55:03 -0800

    Added Documentation

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+## File to setup the environment for training and evaluation of any problem ## 
+# Refer to options.py for definition of values for describing the problem and environment
+
 import os
 import json
 import pprint as pp
@@ -22,7 +25,7 @@ def run(opts):
     # Pretty print the run args
     pp.pprint(vars(opts))
 
-    # Set the random seed
+    # Set the random seed [Parameter Used: opts.seed]
     torch.manual_seed(opts.seed)
 
     # Optionally configure tensorboard
@@ -38,10 +41,11 @@ def run(opts):
     # Set the device
     opts.device = torch.device("cuda:0" if opts.use_cuda else "cpu")
 
-    # Figure out what's the problem
+    # Loads the given problem and its methods [Paramter Used: opts.problem]
+    # Refer to problems/vrp for CVRP problem setup
     problem = load_problem(opts.problem)
 
-    # Load data from load_path
+    # Load data from the option load_path
     load_data = {}
     assert opts.load_path is None or opts.resume is None, "Only one of load path and resume can be given"
     load_path = opts.load_path if opts.load_path is not None else opts.resume
@@ -49,12 +53,19 @@ def run(opts):
         print('  [*] Loading data from {}'.format(load_path))
         load_data = torch_load_cpu(load_path)
 
-    # Initialize model
+    # Initialize model with the required parameters [Parameter Used: opts.model]
+    # We choose to work with attention model
+    # Note: Pointer network is not created for CVRP 
     model_class = {
         'attention': AttentionModel,
         'pointer': PointerNetwork
     }.get(opts.model, None)
+
     assert model_class is not None, "Unknown model: {}".format(model_class)
+
+    # Use the parameters to create model instance [Parameters Used: opts.embedding_dim, opts.hidden_dim, opts.n_encode_layers]
+    # Default values for embedding dimension(opts.embedding_dim) and hidden dimensions(opts.hidden_dim) are 128
+    # Default value for number of encoding layers(opts.n_encode_layers) = 3 
     model = model_class(
         opts.embedding_dim,
         opts.hidden_dim,
@@ -68,14 +79,21 @@ def run(opts):
         shrink_size=opts.shrink_size
     ).to(opts.device)
 
+    # Parallelize model if multiple GPU or GPU cores are available [parameter used opts.use_cuda]
     if opts.use_cuda and torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
 
-    # Overwrite model parameters by parameters to load
+    # In case of saved parameters, overwrite model parameters by parameters to load
     model_ = get_inner_model(model)
     model_.load_state_dict({**model_.state_dict(), **load_data.get('model', {})})
 
-    # Initialize baseline
+    # Initialize baseline [Parameter Used: opts.baseline]
+    """
+        For the experiment we use the following values of opts.baseline
+        :rollout: Performs PPO loss with Greedy Baseline
+        :critic_lstm: Performs PPO loss with Critic LSTM
+    """ 
+    # Note: Critic network is not defined for CVRP problems
     if opts.baseline == 'exponential':
         baseline = ExponentialBaseline(opts.exp_beta)
     elif opts.baseline == 'critic' or opts.baseline == 'critic_lstm':
@@ -113,7 +131,8 @@ def run(opts):
     if 'baseline' in load_data:
         baseline.load_state_dict(load_data['baseline'])
 
-    # Initialize optimizer
+    # Initialize Adam optimizer with given learning rate [Parameter Used: opts.lr_model]
+    # In case of Actor-Critic architecture, initialize Critic learnin rate [Parameter Used: opts.lr_critic]
     optimizer = optim.Adam(
         [{'params': model.parameters(), 'lr': opts.lr_model}]
         + (
@@ -132,13 +151,21 @@ def run(opts):
                 if torch.is_tensor(v):
                     state[k] = v.to(opts.device)
 
-    # Initialize learning rate scheduler, decay by lr_decay once per epoch!
+    # Initialize learning rate scheduler or learning rate decay [Parameter Used: opts.lr_decay]
     lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lambda epoch: opts.lr_decay ** epoch)
 
-    # Start the actual training loop
+    # Create validation dataset for a specific problem instance
+    """
+        Creating dataset for a configurations
+        :param size: Number of nodes in the problem instance (We use 20 for our experiments)
+        :param num_samples: Number of problem instances with given number of nodes (default: 20)
+        :param filename: Filename for the pickle file generated
+        :param distribution: The distribution for sampling coordinates of nodes (default: random uniform)
+    """ 
     val_dataset = problem.make_dataset(
         size=opts.graph_size, num_samples=opts.val_size, filename=opts.val_dataset, distribution=opts.data_distribution)
-
+    
+    # Resuming model training process in case of failure
     if opts.resume:
         epoch_resume = int(os.path.splitext(os.path.split(opts.resume)[-1])[0].split("-")[1])
 
@@ -151,12 +178,21 @@ def run(opts):
         print("Resuming after {}".format(epoch_resume))
         opts.epoch_start = epoch_resume + 1
 
+    # Deciding to use model in train or inference mode [Parameter Used: opts.eval_only]
     if opts.eval_only:
+        # For using any model to validate
         validate(model, val_dataset, opts)
     else:
+        # Training process starts
+
+        # Initialize old log probabilities as None (Old log probilities = Policy in previous iterations)
         old_log_probabilities = None
+        
+        # Run the training loop for given numner of epochs [Parameter Used: opts.epoch_start, opts.n_epochs]
+        # For this experiment we use opts.n_epochs = 10, opts.epoch_start = 0
         for epoch in range(opts.epoch_start, opts.epoch_start + opts.n_epochs):
-          #print('First Try of Old Log Probs', old_log_probabilities)
+
+            # Call the training each epoch function
             old_log_probabilities = train_epoch(
                 model,
                 optimizer,
@@ -170,9 +206,9 @@ def run(opts):
                 old_log_probabilities
             )
             
+            # Saving the old log probabilites 
+            # Note: It is important to detach the old_log_probabilites otherwise training iteration will fail after 1st epoch
             old_log_probabilities = move_to(old_log_probabilities.detach(), opts.device)
-          #print('Second Try of Old Log Probs', old_log_probabilities)
-          # print(old_log_probabilities)
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+## File for training and evaluation of model
 import os
 import time
 from tqdm import tqdm
@@ -12,11 +13,11 @@ from nets.attention_model import set_decode_type
 from utils.log_utils import log_values
 from utils import move_to
 
-
+# Load model based on the architecture of machine
 def get_inner_model(model):
     return model.module if isinstance(model, DataParallel) else model
 
-
+# Validating the model based on validation dataset and reporting average distance
 def validate(model, dataset, opts):
     # Validate
     print('Validating...')
@@ -47,11 +48,11 @@ def rollout(model, dataset, opts):
 
 def clip_grad_norms(param_groups, max_norm=math.inf):
     """
-    Clips the norms for all param groups to max_norm and returns gradient norms before clipping
-    :param optimizer:
-    :param max_norm:
-    :param gradient_norms_log:
-    :return: grad_norms, clipped_grad_norms: list with (clipped) gradient norms per group
+        Clips the norms for all param groups to max_norm and returns gradient norms before clipping
+        :param optimizer: Adams Optimizer with variable learning rate
+        :param max_norm: The maximum value of gradient, given by opts.max_grad_norm
+        :param gradient_norms_log:
+        :return: grad_norms, clipped_grad_norms: list with (clipped) gradient norms per group
     """
     grad_norms = [
         torch.nn.utils.clip_grad_norm_(
@@ -66,24 +67,50 @@ def clip_grad_norms(param_groups, max_norm=math.inf):
 
 
 def train_epoch(model, optimizer, baseline, lr_scheduler, epoch, val_dataset, problem, tb_logger, opts, old_log_likelihood = None):
+    """
+        Training of model for given number of epochs
+        :param model: Attention model selected in run.py
+        :param optimizer: Adams Optimizer with variable learning rate
+        :param baseline: Baseline or Critic chosen to perform experiment
+        :param lr_scheduler: Change the learning rate after each epoch
+        :param epoch: Number of training epochs for each experiment
+        :param val_dataset: Validation dataset used to validate the model after every epoch
+        :param problem: Problem to solve (CVRP)
+        :param tb_logger: Logger to save important value after every few steps 
+        :param opts: Configuration established in the run command or by default values
+        :param old_log_likelihood: Policy for previous iterations                                                                           
+        :return: Likelihood values(Policy) of the current epoch 
+    """
     print("Start train epoch {}, lr={} for run {}".format(epoch, optimizer.param_groups[0]['lr'], opts.run_name))
+
+    # Calculate number of times we train a model on problem instance in each epoch [Parameter Used: opts.epoch_size, opts.batch_size]
     step = epoch * (opts.epoch_size // opts.batch_size)
     start_time = time.time()
 
+    # Connecting model to tensorboard
     if not opts.no_tensorboard:
         tb_logger.log_value('learnrate_pg0', optimizer.param_groups[0]['lr'], step)
 
-    # Generate new training data for each epoch
+    # Generate new training data for each epoch, each trains on a different problem instance for 'step' times
     training_dataset = baseline.wrap_dataset(problem.make_dataset(
         size=opts.graph_size, num_samples=opts.epoch_size, distribution=opts.data_distribution))
     training_dataloader = DataLoader(training_dataset, batch_size=opts.batch_size, num_workers=1)
 
     # Put model in train mode!
     model.train()
+
+    # Training happens in sampling mode to allow the model to explore different paths
+    """
+        Different Sampling modes
+        :sample: Use multinomial distribution to sample node
+        :greedy: Use max probability to sample node
+    """  
     set_decode_type(model, "sampling")
     collect_old_log_likelihoods = []
-    for batch_id, batch in enumerate(tqdm(training_dataloader, disable=opts.no_progress_bar)):
 
+    # Run a particular dataset 'step' times in each epochs
+    for batch_id, batch in enumerate(tqdm(training_dataloader, disable=opts.no_progress_bar)):
+        # Run each step in an epoch
         old_log_like = train_batch(
             model,
             optimizer,
@@ -98,10 +125,11 @@ def train_epoch(model, optimizer, baseline, lr_scheduler, epoch, val_dataset, pr
         )
         collect_old_log_likelihoods.append(old_log_like)
         step += 1
-    print(collect_old_log_likelihoods[-1], len(collect_old_log_likelihoods))
+    # print(collect_old_log_likelihoods[-1], len(collect_old_log_likelihoods))
     epoch_duration = time.time() - start_time
     print("Finished epoch {}, took {} s".format(epoch, time.strftime('%H:%M:%S', time.gmtime(epoch_duration))))
 
+    # Save model after every epoch, allows to load model again if it fails at any given epoch
     if (opts.checkpoint_epochs != 0 and epoch % opts.checkpoint_epochs == 0) or epoch == opts.n_epochs - 1:
         print('Saving model and state...')
         torch.save(
@@ -115,15 +143,19 @@ def train_epoch(model, optimizer, baseline, lr_scheduler, epoch, val_dataset, pr
             os.path.join(opts.save_dir, 'epoch-{}.pt'.format(epoch))
         )
 
+    # Validate the model after completion of epoch on validation datasets
     avg_reward = validate(model, val_dataset, opts)
 
     if not opts.no_tensorboard:
         tb_logger.log_value('val_avg_reward', avg_reward, step)
 
+    # Update the baseline with the newly trained model (only for baseline = rollout)
     baseline.epoch_callback(model, epoch)
 
     # lr_scheduler should be called at end of epoch
     lr_scheduler.step()
+
+    # Select the final policy on a given dataset
     last_iter_log_prob = move_to(collect_old_log_likelihoods[-1], opts.device)
     return last_iter_log_prob
 
@@ -140,16 +172,35 @@ def train_batch(
         opts,
         old_log_likelihood = None
 ):
+    """
+        Training of model for each step in epoch
+        :param model: Attention model selected in run.py
+        :param optimizer: Adams Optimizer with variable learning rate
+        :param baseline: Baseline or Critic chosen to perform experiment
+        :param epoch: Number of training epochs for each experiment
+        :param step: Number of times we train a model on problem instance in each epoch  
+        :param batch_id: Batch id of each batch
+        :param batch: Problem instances in a given batch (default 512) 
+        :param tb_logger: Logger to save important value after every few steps
+        :param opts: Configuration established in the run command or by default values
+        :param old_log_likelihood: Policy for previous iterations                                                                           
+        :return: Likelihood values(Policy) of the current step 
+    """
+
+    # Seperate the inputvalue of batch size and any inital value of cost 
     x, bl_val = baseline.unwrap_batch(batch)
+
+    # Move the variable to device based on whether GPU is available or not [Parameter used: opts.device] 
     x = move_to(x, opts.device)
     bl_val = move_to(bl_val, opts.device) if bl_val is not None else None
-    # Evaluate model, get costs and log probabilities
+    
+    # Evaluate model, get costs and log likelihood
     cost, log_likelihood = model(x)
-    # likelihood = log_likelihood.exp()
 
-    # print(likelihood)
+    # For the first epoch we do not have any old policy, therefore we use the average value of current likelihood as subsitute 
     shapeOfLikelihood = log_likelihood.shape
     if old_log_likelihood == None:
+        # Getting the mean value of likelihood for a given batch
         mean_likelihood = log_likelihood.mean().item()
         old_log_likelihood = np.full(shapeOfLikelihood[0], mean_likelihood)
         old_log_likelihood = torch.as_tensor(old_log_likelihood)
@@ -157,39 +208,46 @@ def train_batch(
     old_log_likelihood = move_to(old_log_likelihood, opts.device)
     log_likelihood = move_to(log_likelihood, opts.device)
 
-    # Evaluate baseline, get baseline loss if any (only for critic)
+    # Evaluate baseline/critic, get baseline/critic loss if any
     bl_val, bl_loss = baseline.eval(x, cost) if bl_val is None else (bl_val, 0)
 
     # Reshape critic output to for faster operations
     bl_val_1 = torch.reshape(bl_val, (-1,))
     
-    # Calculate loss
-    # reinforce_loss = ((cost - bl_val) * log_likelihood).mean()
+    # Calculate Advantage function, this enable algorithm to compare action loss with critic or baseline loss   
     advantage = cost - bl_val
+
+    # Clipping values for PPO loss (default 0.2, selected in paper)
     clip_param = 0.2
-    # if epoch == 0:
-    #   ratio = log_likelihood
-    # else:
-    #   ratio = log_likelihood - old_log_likelihood
 
+    # Define the PPO loss function
+    # Note: Dividing likelihoods can lead to unexpected behavior, therefore we subtract log likelihoods and then exponentiate them 
     ratio = log_likelihood - old_log_likelihood
-    ratio = torch.exp(ratio) #removed exponentiated
+    ratio = torch.exp(ratio)
 
-    surr1 = ratio #removed advantage
-    surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) #removed advantage
-    actor_loss = torch.min(surr1, surr2)  #removed negative and mean
+    surr1 = ratio
+    # Clipping the gradients
+    surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param)
+
+    # Selecting the minimum values between original and clipped gradients
+    actor_loss = torch.min(surr1, surr2)
     
+    # Combining all the values
+    # Note: We add an extra 0.5xbl_loss to allow the LSTM model to update its parameters using backpropogation
     loss = 0.5 * bl_loss + (-(bl_val_1 - cost) * actor_loss).mean() #Added negative sign
 
     # Perform backward pass and optimization step
     optimizer.zero_grad()
     loss.backward()
+
     # Clip gradient norms and get (clipped) gradient norms for logging
     grad_norms = clip_grad_norms(optimizer.param_groups, opts.max_grad_norm)
     optimizer.step()
 
-    # Logging
+    # Logging values after every few steps [parameter used: opts.log_step] 
     if step % int(opts.log_step) == 0:
         log_values(cost, grad_norms, epoch, batch_id, step,
                    log_likelihood, loss, bl_loss, tb_logger, opts)
+
+    # Retunr Likelihood values after each step
     return log_likelihood
